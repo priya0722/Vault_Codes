436df4f6f540264bb600028e65e43d2c92b87a9a
priya0722 <130269958+priya0722@users.noreply.github.com>
2023-08-05 13:25:39 +0530

    Create Student_Class.java

---
+++ Student_Class.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private int rollNumber;
+    private int marks;
+
+    public Student(String name, int rollNumber, int marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+    }
+}
+
